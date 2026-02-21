70c6cc6d67d959e5e6620e666b2165cce18ff3eb
artbot <artbot@artika.top>
2026-02-21 12:35:58 +0000

    fix: Use correct Cloud MQTT field names for Alternator Charger
    Changed from BLE protocol fields to Cloud MQTT fields:
    - batSoc (Battery SOC %)
    - temp (Temperature °C)
    - alternatorPower (Alternator Power W)
    - carBatVolt (Car Battery Voltage V)
    - stationPower (Station Power W)
    - startVoltage (Start Voltage, adjustable 11-30V)
    - permanentWatts (Power Limit, adjustable 0-800W)
    - operationMode (Mode 1-3)
    - startStop (On/Off switch)
    
    Based on ioBroker.ecoflow-mqtt alternator implementation.
    Fixes MQTT data reception for Alternator Charger.

--- custom_components/ecoflow_cloud_alt/devices/public/alternator_charger.py
+++ custom_components/ecoflow_cloud_alt/devices/public/alternator_charger.py
@@ -11,37 +11,39 @@ from custom_components.ecoflow_cloud_alt.sensor import (
     TempSensorEntity,
     InWattsSensorEntity,
     VoltSensorEntity,
-    AmpSensorEntity,
+    RemainSensorEntity,
     StatusSensorEntity
 )
 from custom_components.ecoflow_cloud_alt.switch import EnabledEntity
-from custom_components.ecoflow_cloud_alt.number import MaxBatteryLevelEntity
+from custom_components.ecoflow_cloud_alt.number import MaxBatteryLevelEntity, MinBatteryLevelEntity
 from custom_components.ecoflow_cloud_alt.select import DictSelectEntity
 
 
 class AlternatorCharger(BaseDevice):
     """EcoFlow Alternator Charger device (500W/800W).
     
-    MQTT-only device - does not support REST API quota/all.
+    Cloud MQTT supported device.
+    Based on ioBroker.ecoflow-mqtt alternator implementation.
     """
 
     def sensors(self, client: EcoflowApiClient) -> list[SensorEntity]:
         return [
-            # Battery sensors
-            LevelSensorEntity(client, self, "cms_batt_soc", const.MAIN_BATTERY_LEVEL),
-            TempSensorEntity(client, self, "cms_batt_temp", const.BATTERY_TEMP),
+            # Battery
+            LevelSensorEntity(client, self, "batSoc", const.MAIN_BATTERY_LEVEL),
             
-            # Power and voltage
-            InWattsSensorEntity(client, self, "pow_get_dc_bidi", "DC Power"),
-            VoltSensorEntity(client, self, "sp_charger_car_batt_vol", "Car Battery Voltage"),
+            # Temperature
+            TempSensorEntity(client, self, "temp", const.BATTERY_TEMP),
             
-            # Current limits
-            AmpSensorEntity(client, self, "sp_charger_dev_batt_chg_amp_limit", "Charging Current Limit"),
-            AmpSensorEntity(client, self, "sp_charger_car_batt_chg_amp_limit", "Reverse Charging Current Limit"),
+            # Power
+            InWattsSensorEntity(client, self, "alternatorPower", "Alternator Power"),
+            InWattsSensorEntity(client, self, "stationPower", "Station Power"),
+            InWattsSensorEntity(client, self, "ratedPower", "Rated Power", False),
             
-            # Power limit
-            InWattsSensorEntity(client, self, "sp_charger_chg_pow_limit", "Power Limit"),
-            InWattsSensorEntity(client, self, "sp_charger_chg_pow_max", "Max Power", False),
+            # Voltage
+            VoltSensorEntity(client, self, "carBatVolt", "Car Battery Voltage"),
+            
+            # Time
+            RemainSensorEntity(client, self, "chargeToFull268", "Charging Time", False),
             
             # Status
             StatusSensorEntity(client, self)
@@ -49,79 +51,94 @@ class AlternatorCharger(BaseDevice):
 
     def numbers(self, client: EcoflowApiClient) -> list[NumberEntity]:
         return [
-            # Start voltage (11V - 31V)
+            # Start voltage (11V - 30V, step 0.1V)
             MaxBatteryLevelEntity(
                 client, self, 
-                "sp_charger_car_batt_vol_setting", 
+                "startVoltage", 
                 "Start Voltage",
-                11, 31,
+                11, 30,
                 lambda value: {
                     "sn": self.device_info.sn,
-                    "cmdCode": "WN511_SET_START_VOLTAGE",
-                    "params": {"cfg_sp_charger_car_batt_vol_setting": int(value * 10)}
+                    "operateType": "TCP",
+                    "params": {
+                        "id": 17,
+                        "startVoltage": int(value * 10)
+                    }
                 }
             ),
             
-            # Power limit
+            # Power limit (0W - 800W)
             MaxBatteryLevelEntity(
                 client, self,
-                "sp_charger_chg_pow_limit",
+                "permanentWatts",
                 "Power Limit",
                 0, 800,
                 lambda value: {
                     "sn": self.device_info.sn,
-                    "cmdCode": "WN511_SET_POWER_LIMIT",
-                    "params": {"cfg_sp_charger_chg_pow_limit": int(value)}
+                    "operateType": "TCP",
+                    "params": {
+                        "id": 17,
+                        "permanentWatts": int(value)
+                    }
                 }
             ),
+            
+            # Cable length (0-10m)
+            MinBatteryLevelEntity(
+                client, self,
+                "cableLength608",
+                "Cable Length",
+                0, 10,
+                lambda value: {
+                    "sn": self.device_info.sn,
+                    "operateType": "TCP",
+                    "params": {
+                        "id": 17,
+                        "cableLength608": int(value)
+                    }
+                }
+            )
         ]
 
     def switches(self, client: EcoflowApiClient) -> list[SwitchEntity]:
         return [
-            # Charger on/off
+            # Start/Stop switch
             EnabledEntity(
                 client, self,
-                "sp_charger_chg_open",
+                "startStop",
                 "Charger",
                 lambda value: {
                     "sn": self.device_info.sn,
-                    "cmdCode": "WN511_SET_CHARGER_OPEN",
-                    "params": {"cfg_sp_charger_chg_open": value}
-                }
-            ),
-            
-            # Emergency reverse charging
-            EnabledEntity(
-                client, self,
-                "sp_charger_car_batt_urgent_chg_switch",
-                "Emergency Reverse Charging",
-                lambda value: {
-                    "sn": self.device_info.sn,
-                    "cmdCode": "WN511_SET_EMERGENCY_REVERSE",
-                    "params": {"cfg_sp_charger_car_batt_urgent_chg_switch": value}
+                    "operateType": "TCP",
+                    "params": {
+                        "id": 17,
+                        "startStop": 1 if value else 0
+                    }
                 }
             )
         ]
 
     def selects(self, client: EcoflowApiClient) -> list[SelectEntity]:
-        # Charger modes
+        # Operation modes
         modes = {
-            0: "Idle",
-            1: "Charge (Driving)",
-            2: "Battery Maintenance",
-            3: "Reverse Charge (Parking)"
+            1: "Mode 1",
+            2: "Mode 2",
+            3: "Mode 3"
         }
         
         return [
             DictSelectEntity(
                 client, self,
-                "sp_charger_chg_mode",
-                "Charger Mode",
+                "operationMode",
+                "Operation Mode",
                 modes,
                 lambda value: {
                     "sn": self.device_info.sn,
-                    "cmdCode": "WN511_SET_CHARGER_MODE",
-                    "params": {"cfg_sp_charger_chg_mode": value}
+                    "operateType": "TCP",
+                    "params": {
+                        "id": 17,
+                        "operationMode": value
+                    }
                 }
             )
         ]
